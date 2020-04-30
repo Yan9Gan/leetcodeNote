8162421f4f1b4c194387625ab565a01027fdba37
Ghosque <247350078@qq.com>
2020-04-30 15:00:30 +0800

    202快乐数

---
+++ src/Algorithms/202快乐数/202.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        temp_list = list()
+
+        def count(n):
+            n_list = [int(i) for i in str(n)]
+            sum = 0
+            for item in n_list:
+                sum += item ** 2
+
+            return sum
+
+        while n != 1 and n not in temp_list:
+            temp_list.append(n)
+            n = count(n)
+
+        if n == 1:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    S = Solution()
+    num = 113245
+    result = S.isHappy(num)
+    print(result)
