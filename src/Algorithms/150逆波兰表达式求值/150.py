a25a81170f89a295a7d976f3e4d27e888c74335b
Ghosque <247350078@qq.com>
2020-05-06 14:41:30 +0800

    150逆波兰表达式求值

---
+++ src/Algorithms/150逆波兰表达式求值/150.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from typing import List, Union
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> Union[str, float]:
+        stack = list()
+
+        for item in tokens:
+            if item not in ['+', '-', '*', '/']:
+                stack.append(item)
+            else:
+                second, first = int(stack.pop()), int(stack.pop())
+                print(second, first)
+                if item == '+':
+                    first += second
+                elif item == '-':
+                    first -= second
+                elif item == '*':
+                    first *= second
+                else:
+                    first /= second
+                stack.append(first)
+
+        return stack[0]
+
+
+if __name__ == '__main__':
+    S = Solution()
+    tokens = ["2", "1", "+", "3", "*"]
+    res = S.evalRPN(tokens)
+    print(res)
