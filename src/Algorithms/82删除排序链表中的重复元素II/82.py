aa72d278f7761c64c254794409690658bc34cb71
Ghosque <247350078@qq.com>
2020-05-08 13:47:26 +0800

    delete 82 86 code test case

--- src/Algorithms/82删除排序链表中的重复元素II/82.py
+++ src/Algorithms/82删除排序链表中的重复元素II/82.py
@@ -5,25 +5,6 @@ class ListNode:
         self.next = None
 
 
-def create_list_node(l):
-    list_node = ListNode(None)
-    temp = list_node
-    for item in l:
-        temp.next = ListNode(item)
-        temp = temp.next
-
-    return list_node.next
-
-
-def print_list_node(list_node):
-    s_list = list()
-    while list_node:
-        s_list.append(str(list_node.val))
-        list_node = list_node.next
-
-    print(' -> '.join(s_list))
-
-
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
         if not head:
@@ -52,10 +33,3 @@ class Solution:
             return new.next
         else:
             return None
-
-
-if __name__ == '__main__':
-    S = Solution()
-    list_node = create_list_node([1, 1, 2, 3, 4, 5, 5, 6])
-    res = S.deleteDuplicates(list_node)
-    print_list_node(res)
--- src/Algorithms/86分隔链表/86.py
+++ src/Algorithms/86分隔链表/86.py
@@ -5,25 +5,6 @@ class ListNode:
         self.next = None
 
 
-def create_list_node(l):
-    list_node = ListNode(None)
-    temp = list_node
-    for item in l:
-        temp.next = ListNode(item)
-        temp = temp.next
-
-    return list_node.next
-
-
-def print_list_node(list_node):
-    s_list = list()
-    while list_node:
-        s_list.append(str(list_node.val))
-        list_node = list_node.next
-
-    print(' -> '.join(s_list))
-
-
 class Solution:
     def partition(self, head: ListNode, x: int) -> ListNode:
         new = ListNode(None)
@@ -49,11 +30,3 @@ class Solution:
             last = last.next
 
         return new.next
-
-
-if __name__ == '__main__':
-    S = Solution()
-    list_node = create_list_node([1])
-    x = 0
-    res = S.partition(list_node, x)
-    print_list_node(res)
