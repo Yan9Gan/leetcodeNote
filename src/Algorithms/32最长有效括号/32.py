a6737f081b34b9c49ab654595374d70801f1f0c4
Ghosque <247350078@qq.com>
2020-05-29 16:20:57 +0800

    32最长有效括号

---
+++ src/Algorithms/32最长有效括号/32.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s:
+            return 0
+
+        stack = list()
+        dp = [0 for _ in s]
+
+        index = 0
+        while index < len(s):
+            item = s[index]
+
+            if stack and item==')' and stack[-1][0]=='(':
+                dp[stack[-1][1]] += 1
+                if stack[-1][1]-1 >= 0 and dp[stack[-1][1]-1]:
+                    dp[stack[-1][1]] += dp[stack[-1][1]-1]
+                dp[index] += 1+dp[index-1]
+                if index-stack[-1][1] > 1:
+                    dp[index] += dp[stack[-1][1]]
+                stack.pop(-1)
+            else:
+                stack.append([item, index])
+
+            index += 1
+
+        return max(dp)
+
+
+if __name__ == '__main__':
+    S = Solution()
+    s = '()(())'
+    num = S.longestValidParentheses(s)
+    print(num)
