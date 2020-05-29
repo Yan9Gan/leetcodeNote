3506d35289636f3d2d7bc067a455714e6b626205
Ghosque <247350078@qq.com>
2020-05-29 15:16:31 +0800

    198打家劫舍

---
+++ src/Algorithms/198打家劫舍/198.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [0 for _ in nums]
+        if not nums:
+            return 0
+        if len(nums) >= 1:
+            dp[0] = nums[0]
+        if len(nums) >= 2:
+            dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+
+        return dp[-1]
